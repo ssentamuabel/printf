2ec13f05ee877a1d2ade253996fcc9b25915748d
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-21 13:18:41 -0700

    Printf 5

--- main.h
+++ main.h
@@ -17,6 +17,7 @@ int _printf(const char *format, ...);
 int print_args(const char *format, va_list list);
 int _putchar(char c);
 int _puts(char *str);
+int edgetest(char c);
 int print_char(va_list list);
 int print_string(va_list list);
 #endif
