38b05903e1cdc5910b98e0540a4ad47423077924
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-22 09:53:31 -0700

    printf 7

--- main.h
+++ main.h
@@ -17,7 +17,10 @@ int _printf(const char *format, ...);
 int print_args(const char *format, va_list list);
 int _putchar(char c);
 int _puts(char *str);
+int print_d(va_list list);
+int print_i(va_list list);
 int edgetest(char c);
 int print_char(va_list list);
 int print_string(va_list list);
+int print_percent(va_list list);
 #endif
